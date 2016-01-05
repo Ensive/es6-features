5b5a747e3abfb4dc9a8e65a14ede331e701e1438
Anton Goncharuk <antonge92@gmail.com>
2016-01-05 22:02:36 +0200

    Use interpolation to build string. Update function, add method isActive (using shorthand without function keyword). Write some expectations.

--- app/scripts/objects.js
+++ app/scripts/objects.js
@@ -3,11 +3,19 @@
 ((exports) => {
   'use strict';
 
-  function buildUser(first, last) {
-    let fullName = first + ' ' + last;
+  function buildUser(first, last, postCount) {
+    let fullName = `${first} ${last}`;
+    const ACTIVE_POST_COUNT = 10;
 
     // use shorthand
-    return { first, last, fullName };
+    return {
+      first,
+      last,
+      fullName,
+      isActive() {
+        return postCount >= ACTIVE_POST_COUNT;
+      }
+    };
   }
 
   exports.buildUser = buildUser;
--- test/spec/objects.spec.js
+++ test/spec/objects.spec.js
@@ -6,10 +6,14 @@
 
   describe('buildUser function', function () {
     it('should build a user', function () {
-      let user = buildUser('Sam', 'Williams');
+      let user = buildUser('Sam', 'Williams', 11);
       expect(user.first).to.be.equal('Sam');
       expect(user.last).to.be.equal('Williams');
       expect(user.fullName).to.be.equal('Sam Williams');
+      expect(user.isActive()).to.equal(true);
+
+      let newUser = buildUser('First', 'Last', 5);
+      expect(newUser.isActive()).to.equal(false);
     });
   });
 
